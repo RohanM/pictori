d4454328540b036486988a62e15832212be01332
Rohan Mitchell <rohan@rohanmitchell.com>
2011-11-14 15:53:12 +1100

    Add wrapping div to images, add (currently disabled) code for Masonry.

--- pictori.js
+++ pictori.js
@@ -3,5 +3,13 @@ $(document).ready(function() {
 	for(i in json.results) {
 	    $("<img>").attr("src", json.results[i].thumb).appendTo($("#images"));
 	}
+	$("#images img").wrap('<div class="image" />');
+
+	/* Some of these images take a long time to load, so masonry isn't applied. Disable for now.
+	$("#images").imagesLoaded(function() {
+	    alert("Masonising");
+	    $("#images").masonry({itemSelector: '.image'});
+	});
+	*/
     });
 });
