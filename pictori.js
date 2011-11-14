0decec11ec121b90312599e3bfb5456a1704da60
Rohan Mitchell <rohan@rohanmitchell.com>
2011-11-14 15:20:09 +1100

    Display images ad-hoc.

--- pictori.js
+++ pictori.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     $.getJSON("http://api.twicsy.com/search?q=love&callback=?", function(json) {
-	alert(json.count);
+	for(i in json.results) {
+	    $("<img>").attr("src", json.results[i].thumb).appendTo($("#images"));
+	}
     });
 });
