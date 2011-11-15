625faa51c105f3f1d36fb46c56a24c2775cf2c7c
Rohan Mitchell <rohan@rohanmitchell.com>
2011-11-15 16:36:59 +1100

    Load images into a hidden div, and display them when loaded

--- pictori.js
+++ pictori.js
@@ -1,15 +1,26 @@
 $(document).ready(function() {
-    $.getJSON("http://api.twicsy.com/search?q=love&callback=?", function(json) {
+    $.getJSON("http://api.twicsy.com/search?q=love&sort=date&callback=?", function(json) {
+	// Load each image into #images-loading, a hidden div
 	for(i in json.results) {
-	    $("<img>").attr("src", json.results[i].thumb).appendTo($("#images"));
+	    $("<img>")
+		.attr("src", json.results[i].thumb)
+		.wrap('<div class="image" />').parent()
+		.appendTo($("#images-loading"));
 	}
-	$("#images img").wrap('<div class="image" />');
 
-	/* Some of these images take a long time to load, so masonry isn't applied. Disable for now.
-	$("#images").imagesLoaded(function() {
-	    alert("Masonising");
-	    $("#images").masonry({itemSelector: '.image'});
+	// Hide images that fail to load
+	// TODO: Remove them instead
+	$("#images-loading .image img").error(function() {
+	    $(this).parent().hide();
 	});
-	*/
+
+	// Move loaded images into #images (TODO: and masonrify them)
+	// To confirm: you can multi-call masonry() with new images and it'll gracefully update.
+	$("#images-loading .image").each(function() {
+	    $(this).imagesLoaded(function() {
+		$(this).detach().prependTo($("#images"));
+		//$("#images").masonry({itemSelector: '.image'});
+	    });
+	});
     });
 });
