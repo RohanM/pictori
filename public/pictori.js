fb7f043764bbe6d9900ecaa25d6f0a1f09f1b7f5
Rohan Mitchell <rohan@rohanmitchell.com>
2011-11-17 17:32:04 +1100

    Split menu to both sides. Place credits at bottom.

--- public/pictori.js
+++ public/pictori.js
@@ -1,7 +1,12 @@
-var topics = ['love', 'hate', 'think', 'believe', 'feel', 'wish'];
-// TODO: Should we store the colours here and generate some CSS?
+var topics = {left:  ['love', 'hate', 'think'],
+	      right: ['believe', 'feel', 'wish']};
+
+// Convert topics to [{side: 'left', topic: t}, ...]
+var topics_array = topics['left'].map(function(t) { return {side: 'left', topic: t}; })
+           .concat(topics['right'].map(function(t) { return {side: 'right', topic: t}; }));
 
 var imageIDs = new Array();
+
 
 $(document).ready(function() {
     initPage();
@@ -14,30 +19,32 @@ $(document).ready(function() {
 
 // Set up HTML elements
 function initPage() {
-    var menu = $("#menu");
+    var sidebarList = {left: $("#sidebar-left"), right: $("#sidebar-right")};
+    var sidebars = $(".sidebar");
     var content = $("#content");
 
     // Build menu and loading/loaded images divs
-    for(i in topics) {
-	var topic = topics[i];
-	$('<a href="#" class="'+topic+'" title="i '+topic+'">').html(topic).appendTo($("<li>")).parent().appendTo(menu);
+    for(i in topics_array) {
+	var side = topics_array[i]['side'];
+	var topic = topics_array[i]['topic'];
+	$('<a href="#" class="'+topic+'" title="i '+topic+'">').html(topic).appendTo($("<li>")).parent().appendTo(sidebarList[side]);
 	$('<div id="images-loading-'+topic+'" class="images-loading" />').appendTo(content);
 	$('<div id="images-loaded-'+topic+'" class="images-loaded" />').appendTo(content);
     }
 
-    // Wire up menu events, select first
-    menu.find("a").click(function() {
-	menu.find("a.selected").removeClass("selected");
+    // Wire up menu events and select the first menu item
+    sidebars.find("a").click(function() {
+	sidebars.find("a.selected").removeClass("selected");
 	$(this).addClass("selected");
 	loadFreshImages();
 	return false;
     });
-    menu.find("a:first").click();
+    sidebarList['left'].find("a:first").click();
 }
 
 // Transfer one loaded image from #images-loaded into #images
 function displayImage() {
-    var topic = $("#menu a.selected").html();
+    var topic = $(".sidebar a.selected").html();
 
     if($("#images-loaded-"+topic+" .image").length > 0) {
 	var image = $("#images-loaded-"+topic+" .image:first");
@@ -48,9 +55,6 @@ function displayImage() {
 	    $("#images").masonry('appended', image);
 	}
 
-	// Scroll to bottom - can we do this without annoyingly taking over the browser?
-	//$('html, body').stop().animate({ scrollTop: $(document).height() }, 1500);
-
 	console.log("Images in queue: "+$("#images-loaded-"+topic+" .image").length);
     }
 }
@@ -58,8 +62,8 @@ function displayImage() {
 
 // Search Twicsy for images and load them into the hidden #images-loaded div
 function loadFreshImages() {
-    var topic = $("#menu a.selected").html();
-    var topic_uri = encodeURIComponent($("#menu a.selected").attr('title'));
+    var topic = $(".sidebar a.selected").html();
+    var topic_uri = encodeURIComponent($(".sidebar a.selected").attr('title'));
 
     $.getJSON("http://api.twicsy.com/search?q="+topic_uri+"&sort=date&callback=?", function(json) {
 	// Load each image into #images-loading-xyz, a hidden div
